57b160685c6077d95e4470f38121fe5a77e9b48b
Paul Woolcock <paul@woolcock.us>
2015-01-06 14:33:35 -0500

    need some diagnostics

--- wren-sys/build.rs
+++ wren-sys/build.rs
@@ -1,12 +1,23 @@
 use std::io::{fs, Command};
+use std::io::fs::PathExtensions;
 use std::os;
 use std::io::process::InheritFd;
 
 fn main() {
     let manifest_dir = Path::new(os::getenv("CARGO_MANIFEST_DIR").unwrap());
+    println!("manifest_dir: {}", manifest_dir.display());
     let out_dir = Path::new(os::getenv("OUT_DIR").unwrap());
+
     let src_dir = manifest_dir.join("src");
+    if !src_dir.exists() {
+        panic!("Source directory {} does not exist", src_dir.display());
+    }
+
     let wren_dir = src_dir.join("wren");
+    if !wren_dir.exists() {
+        panic!("Wren directory {} does not exist", wren_dir.display());
+    }
+
     let wren_lib = wren_dir.join("libwren.a");
 
     let mut make = Command::new("make");
